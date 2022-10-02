3200ec20669592c779445477018f47cff5617e35
ang <ang@ang.hu>
2022-10-02 11:30:38 +0200

    Added github auth

---
+++ formservice/src/main/java/com/example/googleform/security/Security.java
@@ -0,0 +1,47 @@
+package com.example.googleform.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+@Configuration
+public class Security extends WebSecurityConfigurerAdapter {
+
+    @Value("${spring.security.oauth2.client.registration.github.client-id}") String clientId;
+    @Value("${spring.security.oauth2.client.registration.github.client-id}") String clientSecret;
+
+    //@Bean
+    public ClientRegistrationRepository clientRegistrationRepository(){
+        return new InMemoryClientRegistrationRepository(getGithubClientReg());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+                //.clientRegistrationRepository(clientRegistrationRepository());
+    }
+
+//    @Bean
+//    public OAuth2AuthorizedClientService authorizedClientService() {
+//
+//        return new InMemoryOAuth2AuthorizedClientService(
+//                clientRegistrationRepository());
+//    }
+
+    private ClientRegistration getGithubClientReg(){
+        return CommonOAuth2Provider.GITHUB.getBuilder("github")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
+    }
+}
