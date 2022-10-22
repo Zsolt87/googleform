ae3e66347c2fbe6c698e2bc31dcb95981bda1fdd
ang <ang@ang.hu>
2022-10-22 12:38:57 +0200

    Added integration test health check

--- googleformintegrationtest/src/test/java/com/example/googleform/steps/envsetup/EnvSetupSteps.java
+++ googleformintegrationtest/src/test/java/com/example/googleform/steps/envsetup/EnvSetupSteps.java
@@ -3,16 +3,44 @@ package com.example.googleform.steps.envsetup;
 import com.example.googleform.RunTests;
 import io.cucumber.java.en.Given;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
+@Slf4j
 public class EnvSetupSteps {
 
     HttpResponse response;
+    private static final List<Integer> waitingTime = List.of(10,5,5,2,1,1);
+
+    @SneakyThrows
+    @Given("the service is running")
+    public void the_service_is_running() {
+        HttpRequest request =
+                HttpRequest
+                        .newBuilder()
+                        .uri(new URI(RunTests.HOST + "/actuator/health"))
+                        .GET()
+                        .build();
+
+        for (Integer i: waitingTime) {
+            HttpResponse response =
+                    HttpClient
+                            .newHttpClient()
+                            .send(request, HttpResponse.BodyHandlers.ofString());
+
+            if(response.statusCode() == 200)
+                break;
+
+            log.info("Waiting for {} sec", i);
+            Thread.sleep(i*1000);
+        }
+    }
 
     @SneakyThrows
     @Given("the environment is ready")
